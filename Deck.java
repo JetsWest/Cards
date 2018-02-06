75c890895da2dcd338dd3e160dde6d2c5c2088f4
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-02-06 08:48:52 -0800

    Add files via upload

---
+++ Deck.java
@@ -0,0 +1,13 @@
+package pkg21game;
+
+public class Deck {
+    private Card[] cards;
+    public void setDeck() {
+        cards = new Card[52];
+
+    }
+    public void shuffle(){}
+    public void deal(){}
+    public void printCard(){}
+    
+}
