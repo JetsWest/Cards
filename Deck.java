a0f36f2111129c76c7e275218dd76d47293a4699
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-02-23 13:01:18 -0800

    Add files via upload

--- Deck.java
+++ Deck.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class Deck {
     private Card[] cards;
     private int marker;
-    private int loc;
 
     public Deck() {
         this.cards = new Card[52];
@@ -14,12 +13,13 @@ public class Deck {
             for (Card.Rank rank: Card.Rank.values()){
                 this.cards[pos] = new Card(rank, suit);
                 pos++;
-            }
         }
-        marker = 0;
     }
+    marker = 0;
+}
+
     public void shuffle(){
-        //Shuffles the order of the cards in this.cards
+        //Shuffles the order of the cards in this.cards 
         //Also re-introduces all 'drawn' cards back into deck
         int index;
         Card temp;
@@ -31,13 +31,13 @@ public class Deck {
             this.cards[index] = temp;
         }
     }
-
+    
     public Card drawCard() throws NullPointerException{
         //Draws "top" card off the deck, then discards the card
         if (marker == 0){
-            this.shuffle();
+        this.shuffle();
         }
-
+        
         if (hasNext()){
             Card res = this.cards[marker];
             marker++;
@@ -73,17 +73,59 @@ public class Deck {
         for (int i = 0; i < this.cards.length-1; i++){
             int pos = i;
             for (int j = i+1; j < this.cards.length; j++){
-                if (this.cards[j].getRank().value < this.cards[pos].getRank().value && i != j){
-                        pos = j;
-                        swap(pos, i);
-                }
+               if (this.cards[j].getRank().value < this.cards[pos].getRank().value){
+                   pos = j;
+               }
+                swap(pos, i);
             }
         }
     }
     public void insertionSort(){
-
+        for (int i = 0; i < this.cards.length; i++){
+            for (int k = i; k >= 1 && this.cards[k].getRank().value < this.cards[k-1].getRank().value; k--){
+                swap (k, k-1);
+            }
+        }
     }
     public void mergeSort(){
 
+    }
+    private Card[] mergeSortR(Card[] list){
+        //Stops an infinite recursion from happening
+        //based on idea that list of 1 or less things is already sorted
+        if (cards.length <= 1){
+            return cards;
+        }
+        
+        //Spltting lists
+        Card[] left = new Card[cards.length/2]; 
+        for (int i = 0; i < cards.length/2; i++){
+            left[i] = cards[i];
+        }
+        Card[] right = new Card[cards.length-left.length];
+        for (int i = left.length; i < cards.length; i++){
+            right[i - left.length] = cards[i];
+        }
+        
+        //Sorting
+        left = mergeSortR(left);
+        right = mergeSortR(right);
+    
+        //Combining
+        Card[] temp = new Card[left.length+right.length]; //STEP ONE
+        int newMark = 0; //STEP TWO
+        int lMark = 0;
+        int rMark = 0;
+        
+        while (lMark < left.length && rMark < right.length){
+            if (left[lMark].getRank().value < right[rMark].getRank().value){
+                temp[newMark++] = left[lMark++];
+            }
+            if (right[rMark].getRank().value < left[lMark].getRank().value){
+                temp[newMark++] = right[rMark++];
+            }
+        }
+        System.out.println(temp);
+        return temp;
     }
 }
